2739b09707f0468b1b7bc37a8a3f5e00808141d3
Andre Bonizi <andrebonizi@gmail.com>
2021-03-20 22:16:01 -0300

    footer structure

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+    let socialNets = [
+        { title:"Facebook", link:""},
+        { title:"Email", link:""},
+        { title:"Youtube", link:""},
+        { title:"Soundcloud", link:""},
+        { title:"Instagram", link:""},
+        { title:"Web", link:""},
+        
+    ]
+</script>
+
+<div>
+    Footer
+</div>
