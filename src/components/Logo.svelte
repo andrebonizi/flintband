91c547b7281b564df7743b41cf9217ea0c79750a
Andre Bonizi <andrebonizi@gmail.com>
2021-03-20 22:14:59 -0300

    new logo

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,8 +1,19 @@
-<img src="image/logo.png" />
+<script>
+    export let mobile;
+</script>
+<div>
+    <img class="{mobile? "mobile":"desk"}" src="/image/logo_Mistral.png" alt="flint band logo" />
+</div>
 
 <style>
-    img{
-        width: 50%;
-        height: 50%;
+    .desk{
+        width: 40vw;
+    }
+    .mobile{
+        width: 50vw;
+        position: fixed;
+        left: 0;
+        top: 0;
+        z-index: 2;
     }
 </style>
