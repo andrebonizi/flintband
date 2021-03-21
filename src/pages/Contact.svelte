e896ba6c39d7c33b3512bcf2849f5ab7cd9e34ad
Andre Bonizi <andrebonizi@gmail.com>
2021-03-20 22:13:21 -0300

    contact content and transition

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -1,17 +1,21 @@
-<div class="container">
+<script>
+    import {fly} from 'svelte/transition';
+</script>
+
+<div in:fly="{{ x: 200, duration: 2000 }}" class="container">
     <div>
         <p>
-            Email:<br>
+            <b>Email:</b><br>
             <a href="mailto:flintbrisbane@gmai.com">flintbrisbane@gmail.com</a>
         </p>
         <p>
-            Telephones:<br><br>
+            <b>Telephones:</b><br>
             +61 0410 391 913<br>
             +61 0490 116 835<br>
         </p>
         <p>
-            Smoke signals:
-            <img src="" alt="smoke signals" />
+            <b>Smoke signals:</b><br>
+            <img src="image/smokesignals.png" alt="smoke signals" />
         </p>
     </div>
 </div>
@@ -20,5 +24,17 @@
     .container{
         display: flex;
         flex-direction: column;
+    }
+    p{
+        font-size: 1.5rem;
+        line-height: 1.5rem;
+        font-weight: normal;
+    }
+    b{
+        text-decoration: underline;
+        line-height: 3rem;
+    }
+    img{
+        width: 100%;
     }
 </style>
