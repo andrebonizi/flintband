3421718f1325a50d6a3fba5de9e486e0cf97b144
Andre Bonizi <andrebonizi@gmail.com>
2021-03-20 22:13:56 -0300

    home content and transition

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,25 @@
-<div>
+<script>
+    import { fly } from 'svelte/transition'; 
+    import Logo from '../components/Logo.svelte';
+    export let mobile;
+</script>
+
+<div in:fly="{{ x: 200, duration: 2000 }}">
+    {#if mobile}
+        <Logo />
+    {/if}
     <p>
-        Welcome to Flint band's website!
+        Flint is a Brisbane based band that want to be heard.
+        <br><br>
+        Our sound speaks for itself.
+        <br>
     </p>
 </div>
+
+<style>
+    p{
+        text-align: center;
+        font-size: 1.5rem;
+        line-height: 50px;
+    }
+</style>
