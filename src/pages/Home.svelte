6e766fd0d395be31a5e96234b18b549c0ee999b2
Andre Bonizi <andrebonizi@gmail.com>
2021-03-25 23:51:20 -0300

    home changes

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -6,20 +6,39 @@
 
 <div in:fly="{{ x: 200, duration: 2000 }}">
     {#if mobile}
-        <Logo />
+        <img src="image/photos/0.jpg" alt="Flint" />
     {/if}
-    <p>
-        Flint is a Brisbane based band that want to be heard.
+    <div class="content">
+        <div class="flint">Flint.</div> is a Brisbane-based band that want to be heard.
         <br><br>
         Our sound speaks for itself.
         <br>
-    </p>
+    </div>
 </div>
 
 <style>
-    p{
+    @font-face {
+        font-family: 'Mistral';
+        font-style: normal;
+        font-weight: normal;
+        src: local('Mistral'), url('/fonts/MISTRAL.woff') format('woff');
+    }
+
+    .content{
+        padding-top: 100px;
         text-align: center;
-        font-size: 1.5rem;
+        font-size: 2rem;
         line-height: 50px;
+    }
+    img{
+        width: 100%;
+    }
+
+    .flint{
+        display: inline;
+        font-family: 'Mistral';
+        padding: 0;
+        margin: 0;
+        font-size: 3rem;
     }
 </style>
