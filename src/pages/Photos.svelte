c881c4aa5ebb25dee98fd240833babec5241eb4d
Andre Bonizi <andrebonizi@gmail.com>
2021-04-01 22:59:35 -0300

    add home photos to photo session

--- src/pages/Photos.svelte
+++ src/pages/Photos.svelte
@@ -5,8 +5,10 @@
 
     let path = "image/photos/";
     let photos = [
+        "studio_all_of_us.jpg",    
         "0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg",
         "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg",
+        
     ];
     $: left = 0;
 
