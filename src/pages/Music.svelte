cc8e06ac8c1d91d79fb796e35b835317b82fe534
Imac <imac@iMac-de-Imac.local>
2021-06-18 08:59:44 -0300

    music page style and link icons

--- src/pages/Music.svelte
+++ src/pages/Music.svelte
@@ -6,11 +6,44 @@
 
 <div in:fly="{{ x: 200, duration: 2000 }}">
     <div class="info">
-        Coming soon...
+        Click on preferred link to listen to the song or see the video.
     </div>
     <div  class="{ mobile ? "mobile": "desk"}">
-        <img src="image/covers/embers.png" alt="embers album cover" />
-        <img src="image/covers/colours.png" alt="colours album cover" />
+        <div class="single">
+            <img src="image/covers/embers.png" alt="embers album cover" />
+            <div class="links">
+                <img class="icon" src="image/icons/youtube.png" alt="youtube icon" />
+                <img class="icon" src="image/icons/spotify.png" alt="spotify icon" />
+                <img class="icon" src="image/icons/soundcloud.png" alt="soundcloud icon" />
+            </div>
+            <div>
+                Colours begins with a sparkley guitar
+                to ease into the mood, lifts your
+                spirit in the chorus, and finishes big
+                when 
+                <div class="highlight">
+                    All The Colours
+                    Blend Into One.
+                </div>
+            </div>
+        </div>
+        <div class="single">
+            <img src="image/covers/colours.png" alt="colours album cover" />
+            <div class="links">
+                <img class="icon" src="image/icons/youtube.png" alt="youtube icon" />
+                <img class="icon" src="image/icons/spotify.png" alt="spotify icon" />
+                <img class="icon" src="image/icons/soundcloud.png" alt="soundcloud icon" />
+            </div>
+            <div>
+                Follow the Embers is
+                <div class="highlight">
+                    Flint.’s     
+                </div> 
+                first
+                EP, out late July this year.
+            </div>
+
+        </div>
     </div>
 </div>
 <style>
@@ -26,11 +59,29 @@
         text-align: center;
     }
     img{
-        width: 250px;
+        width: 100%;
+    }
+    .single{
         margin: 20px 50px;
+        width: 250px;
+    }
+    .single > div{
+        font-size: 1rem;
+    }
+    .icon{
+        width: 50px;
+    }
+    div.highlight{
+        display: inline;
+        font-size: 2rem;
+    }
+    .links{
+        display: flex;
+        justify-content: center;
+        align-content: center;
     }
     .info{
-        font-size: 2rem;
+        font-size: 1.5rem;
         display: flex;
         justify-content: center;
         margin-top: 20px;
